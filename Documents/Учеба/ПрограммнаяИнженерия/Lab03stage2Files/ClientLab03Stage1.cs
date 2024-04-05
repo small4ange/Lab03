807905d1c5a1226551097f5e45ffe152a2f95c29
small4ange <katrinavolk37695@gmail.com>
2024-04-05 23:02:57 +0300

    Добавление client и server в stage1

---
+++ Documents/Учеба/ПрограммнаяИнженерия/Lab03stage2Files/ClientLab03Stage1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (TcpClient client = new TcpClient("127.0.0.1", 8000))
+        using (NetworkStream stream = client.GetStream())
+        {
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+
+            while (true)
+            {
+                string userInput = "";
+                Console.Write("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): > ");
+                string command = Console.ReadLine();
+                userInput += command;
+                if (command == "exit")
+                {
+                    writer.WriteLine(userInput);
+                    Console.WriteLine("The request was sent.");
+                    reader.Close();
+                    writer.Close();
+                    client.Close();
+                    stream.Close();
+                    break;
+                }
+                Console.Write("Enter filename: > ");
+                string filename = Console.ReadLine();
+                userInput += "|" + filename;
+
+                if (command == "1") //GET
+                {
+                    //отправляем запрос на сервер
+                    writer.WriteLine(userInput);
+                    Console.WriteLine("The request was sent.");
+
+                    //Считывание ответа сервера 200 или 404
+                    string response = reader.ReadLine();
+
+                    if (response == "200")
+                    {
+                        Console.WriteLine("The content of the file is: " + reader.ReadLine());
+                    }
+                    else if (response == "404")
+                    {
+                        Console.WriteLine("The response says that the file was not found!");
+                    }
+                }
+                else if (command == "2") //PUT
+                {
+                    //запрашиваем у клиента текст для файла
+                    Console.Write("Enter file content: > ");
+                    string content = Console.ReadLine();
+                    userInput += "|" + content;
+
+                    //отправляем запрос на сервер
+                    writer.WriteLine(userInput);
+                    Console.WriteLine("The request was sent.");
+
+                    //Считывание ответа сервера 200 или 403
+                    string response = reader.ReadLine();
+                    Console.WriteLine(response);
+                    if (response == "200")
+                    {
+                        Console.WriteLine("The response says that file was created!");
+                    }
+                    else if (response == "403")
+                    {
+                        Console.WriteLine("The response says that creating the file was forbidden!");
+                    }
+
+                }
+                else if (command == "3")
+                {//DELETE
+                    //отправляем запрос на сервер
+                    writer.WriteLine(userInput);
+                    Console.WriteLine("The request was sent.");
+
+                    //Считывание ответа сервера 200 или 404
+                    string response = reader.ReadLine();
+
+                    if (response == "200")
+                    {
+                        Console.WriteLine("The response says that the file was succesfully deleted");
+                    }
+                    else if (response == "404")
+                    {
+                        Console.WriteLine("The response says that the file was not found!");
+                    }
+                } else { }
+            }
+            
+        }
+    }
+}
+
---
+++ Documents/Учеба/ПрограммнаяИнженерия/Lab03stage2Files/ServerLab03Stage1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class ServerLab03
+{
+    static void Main(string[] args)
+    {
+        TcpListener server = null;
+
+        server = new TcpListener(IPAddress.Parse("127.0.0.1"), 8000); //создаем листенер
+        server.Start();//запускаем сервер
+        Console.WriteLine(" Server started! ");
+
+        string folderPath = @"C:\Users\Katka\source\repos\ServerLab03\ServerLab03\Lab03Files";
+        
+        using (TcpClient client = server.AcceptTcpClient())
+        using (NetworkStream stream = client.GetStream())
+        {
+            //потоки для получения и отправки информации
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+
+            //Console.WriteLine("Client connected. Waiting for command...");
+
+            while (true)
+            {
+                
+                string[] userInput = reader.ReadLine().Split('|');
+                Console.WriteLine(userInput[0]);
+
+                string command = userInput[0];//считываем команду от клиента
+                //Проверка на окончание работы сервера
+                if (command == "exit")
+                {
+                    writer.Close();
+                    reader.Close();
+                    client.Close();
+                    server.Stop();
+                    break;
+                }
+                string filename = userInput[1];// имя файла клиента
+                string fullPath = Path.Combine(folderPath, filename);
+                Console.WriteLine($"{filename}");
+
+                //switch case для ответа на команду клиента
+                switch (command)
+                {
+                    case "1": //GET
+                        if (!File.Exists(fullPath)) { writer.WriteLine("404"); }
+                        else
+                        {
+                            writer.WriteLine("200");
+                            writer.WriteLine(File.ReadAllText(fullPath));
+
+                        }
+                        break;
+
+                    case "2": //PUT
+                        string content = userInput[2];
+
+                        Console.WriteLine(fullPath);
+
+                        try
+                        {
+                            if (File.Exists(fullPath)) { writer.WriteLine("403"); }
+                            else
+                            {
+                                File.WriteAllText(fullPath, content);
+                                writer.WriteLine("200");
+                            }
+
+                        }
+                        catch
+                        {
+                            writer.WriteLine("403");
+                        }
+                        break;
+
+                    case "3": //DELETE
+                        if (!File.Exists(fullPath))
+                        {
+                            writer.WriteLine("404");
+                        }
+                        else
+                        {
+                            try
+                            {
+                                File.Delete(fullPath);
+                                writer.WriteLine("200");
+                            }
+                            catch
+                            {
+                                writer.WriteLine("404");
+                            }
+                        }
+                        break;
+
+                }
+
+            }
+
+        }
+    }
+}
